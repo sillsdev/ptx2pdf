5bfd4fdcecb6e720f41067287d836eba2f5401e6
Mark Penny <mark_penny@sil.org>
2020-06-19 19:23:09 +0530

    Add gspawn-win64-helper to installer

--- Build4Windows.bat
+++ Build4Windows.bat
@@ -21,7 +21,7 @@ REM Then use a python script to build the #include list of only the needed icons
 python python\scripts\getstockicons -f inno -s dist\ptxprint -d "{app}" -i list-remove-symbolic.symbolic -i list-add-symbolic.symbolic -i pan-down-symbolic.symbolic python\lib\ptxprint\ptxprint.glade AdwaitaIcons.txt
 
 REM Then call InnoSetup to build the final SetupPTXprint.exe file which is distributed
-IF "%INNOSETUP_PATH%"=="" SET INNOSETUP_PATH="C:\Program Files (x86)\Inno Setup 6"
-"%INNOSETUP_PATH%\ISCC.exe" InnoSetupPTXprint.iss
+IF %INNOSETUP_PATH%=="" SET INNOSETUP_PATH="C:\Program Files (x86)\Inno Setup 6"
+%INNOSETUP_PATH%\ISCC.exe InnoSetupPTXprint.iss
 
 echo Windows Installer executable file is located in "Output" folder
