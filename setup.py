0994c3741e4641682db311bddb0075f2dfc52053
Nicolas Spalinger <nicolas.spalinger@sil.org>
2020-06-30 14:52:09 +0200

    Updated Ubuntu packaging elements.

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-""" py3.8 Ubuntu focal test packaging """
+""" py3.8 Ubuntu focal packaging """
 
 from glob import glob
 from setuptools import setup, find_packages
 
 setup(
     name="PTXprint",
-    version="0.8.2",
+    version="0.8.6",
     description="Typesetting using (Xe)TeX for ParaText",
     url="https://software.sil.org/ptxprint",
-    maintainer="SIL International",
+    author="SIL International",
     packages=find_packages('python/lib'),  # include all packages under lib
     package_dir={'': 'python/lib'},  # indicate packages are under lib
     include_package_data=True,  # include everything in MANIFEST.in
     package_data={'ptxprint': ['*.*']},
-    #data_files=[('ptx2pdf/src', glob('src/*.tex')),
-    #            ('ptx2pdf/src/mappings', glob('src/mappings/*.tec'))],
-    scripts=["python/scripts/ptxprint", "python/scripts/xdvitype"],
+    scripts=["python/scripts/ptxprint", "python/scripts/xdvitype", "python/scripts/usfmerge"],
     install_requires=["regex", "pygobject", "fonttools", "pycairo"],
     zip_safe=False,
     license="MIT",
