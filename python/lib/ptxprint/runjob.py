8f6d70d6da836460bdee7674c5a3f2a2bdb3a33f
Mark Penny <mark_penny@sil.org>
2020-07-28 18:29:34 +1200

    piclists working again [nobuild]

--- python/lib/ptxprint/runjob.py
+++ python/lib/ptxprint/runjob.py
@@ -577,7 +577,7 @@ class RunJob:
         def carefulCopy(p, src, tgt):
             ratio = pageRatios[1 if p['size'].startswith("span") else 0]
             return self.carefulCopy(ratio, src, tgt)
-        missingPics = self.printer.generateNProcPiclists(jobs, os.path.join(self.printer.working_dir, "tmpPicLists"), carefulCopy, isTemp=True)
+        missingPics = self.printer.generateNProcPicLists(jobs, os.path.join(self.printer.working_dir, "tmpPicLists"), carefulCopy, isTemp=True)
             
         if len(missingPics):
             missingPicList += ["{}".format(", ".join(list(set(missingPics))))]
