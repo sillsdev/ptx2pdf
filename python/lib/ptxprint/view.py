84efe6ef7150b36ee8be8f2253018fc5b8326f26
Mark Penny <mark_penny@sil.org>
2020-08-12 21:49:20 +1200

    Bump to 0.9.3 after a bunch of bug fixes pre 1.0 release

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -10,7 +10,7 @@ from io import StringIO
 import datetime, time
 from shutil import copyfile, copytree
 
-VersionStr = "0.9.2 beta"
+VersionStr = "0.9.3 beta"
 
 pdfre = re.compile(r".+[\\/](.+)\.pdf")
 
