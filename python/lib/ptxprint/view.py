d8d57a9d229381940e07c36203c8266e97a13c12
Martin Hosken <martin_hosken@sil.org>
2020-09-21 17:37:55 +0700

    refkey now handles L/R

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -33,9 +33,9 @@ def newBase(fpath):
 def refKey(r, info=""):
     m = re.match(r"^(\D*)\s*(\d*)\.?(\d*)(\S*?)$", r)
     if m:
-        return (books.get(m.group(1), "100"), int(m.group(2) or 0), int(m.group(3) or 0), info, m.group(4))
+        return (books.get(m.group(1)[:3], "100"), m.group(1)[3:], int(m.group(2) or 0), int(m.group(3) or 0), info, m.group(4))
     else:
-        return (r, 0, 0, info)
+        return (r, "", 0, 0, info)
 
 class Path(pathlib.Path):
 
