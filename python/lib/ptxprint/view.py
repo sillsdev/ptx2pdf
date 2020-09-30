0b7a891116153ed457776a89aff9e9cd5dd7d717
Mark Penny <mark_penny@sil.org>
2020-09-30 22:18:49 +1300

    Bump to 1.1.9.6 for testing

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -11,7 +11,7 @@ from io import StringIO
 import datetime, time
 from shutil import copyfile, copytree, move
 
-VersionStr = "1.1.9.5"
+VersionStr = "1.1.9.6"
 
 pdfre = re.compile(r".+[\\/](.+)\.pdf")
 
