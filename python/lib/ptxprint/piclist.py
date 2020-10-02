4e8f5dd88c4405edd840cc1979034ac62cacbe87
Martin Hosken <martin_hosken@sil.org>
2020-10-02 10:47:54 +0700

    Fix clearing the piclist editor list

--- python/lib/ptxprint/piclist.py
+++ python/lib/ptxprint/piclist.py
@@ -39,7 +39,7 @@ class PicList:
         for k, v in _form_structure.items():
             w = builder.get_object(v)
             w.connect("value-changed" if v[0].startswith("s_") else "changed", self.item_changed, k)
-        # self.clear()
+        self.clear()
 
 
     def modify_font(self, p):
