97462a7b79210aa1edcd8d18ac3cf12201a9a408
Martin Hosken <martin_hosken@sil.org>
2020-10-06 11:43:17 +0700

    expand_module does basic expansion

--- python/lib/ptxprint/usfmutils.py
+++ python/lib/ptxprint/usfmutils.py
@@ -141,6 +141,8 @@ class Usfm:
                 if pred(e):
                     a.append(sfm.Text(e, e.pos, a or None))
                 return a
+            if e.name in removes:
+                return a
             e_ = sfm.Element(e.name, e.pos, e.args, parent=a or None, meta=e.meta)
             reduce(_g, e, e_)
             if pred(e):
