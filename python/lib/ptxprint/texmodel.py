c2768839be39145a6e2534097fe639b2b916cac4
Martin Hosken <martin_hosken@sil.org>
2020-10-20 19:36:27 +0700

    Revert need for lastbook when we have lastptxfile

--- python/lib/ptxprint/texmodel.py
+++ python/lib/ptxprint/texmodel.py
@@ -643,7 +643,7 @@ class TexModel:
                         if extra != "":
                             fname = re.sub(r"^([^.]*).(.*)$", r"\1"+extra+r".\2", fname)
                         if i == len(self.dict['project/bookids']) - 1 and self.dict['project/ifcolophon'] == "":
-                            res.append("\\lastbooktrue\n\\endbooknoejecttrue\n")
+                            res.append("\\lastptxfiletrue\n\\endbooknoejecttrue\n")
                         if self.asBool('document/ifomitsinglechnum') and \
                            self.asBool('document/showchapternums') and \
                            f in oneChbooks:
