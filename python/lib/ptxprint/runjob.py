a146d72be779749503695323c2f960caad73bf90
Martin Hosken <martin_hosken@sil.org>
2020-11-02 16:42:42 +0700

    Fix xetex running to stop on first error and also nonstopmode

--- python/lib/ptxprint/runjob.py
+++ python/lib/ptxprint/runjob.py
@@ -430,7 +430,7 @@ class RunJob:
             self.printer.incrementProgress()
             if info["document/toc"] != "%":
                 tocdata = self.readfile(os.path.join(self.tmpdir, outfname.replace(".tex", ".toc")))
-            cmd = ["xetex", "--halt-on-error", "-no-pdf"]
+            cmd = ["xetex", "-halt-on-error", "-interaction=nonstopmode", "-no-pdf"]
             runner = call(cmd + [outfname], cwd=self.tmpdir, logbuffer=logbuffer)
             if isinstance(runner, subprocess.Popen) and runner is not None:
                 try:
