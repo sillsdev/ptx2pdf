f878addadef9d395ec666aaf7153a037f452cb0f
Mark Penny <mark_penny@sil.org>
2020-11-28 00:31:17 +1300

    Bump version to 1.4.1 for feedback from UA testers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTXprint",
-    version="1.4",
+    version="1.4.1",
     description="Typesetting using (Xe)TeX for Paratext",
     url="https://software.sil.org/ptxprint",
     author="SIL International",
