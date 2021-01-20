6212af2b3596d553cbd52bec3c89bd23cd56c733
Mark Penny <mark_penny@sil.org>
2021-01-20 18:03:22 +1300

    Release 1.5.5 with fix to multi-threaded exception issue

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -15,7 +15,7 @@ from shutil import rmtree
 import datetime, time
 from shutil import copyfile, copytree, move
 
-VersionStr = "1.5.4"
+VersionStr = "1.5.5"
 
 pdfre = re.compile(r".+[\\/](.+)\.pdf")
 
