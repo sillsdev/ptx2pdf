9a3c21197e8a029a721da169f98e40772658e823
Mark Penny <mark_penny@sil.org>
2021-01-21 23:10:14 +1300

    Release 1.5.6 with lots of bug fixes and minor improvements

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -15,7 +15,7 @@ from shutil import rmtree
 import datetime, time
 from shutil import copyfile, copytree, move
 
-VersionStr = "1.5.5"
+VersionStr = "1.5.6"
 
 pdfre = re.compile(r".+[\\/](.+)\.pdf")
 
