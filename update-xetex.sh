88dd81a1db5f243e794a277e6832a63ef4da1d39
Bobby de Vos <bobby_devos@sil.org>
2021-01-26 17:06:13 -0700

    Trim list of needed packages
    The initial list comes from the Minimal installation section
    of http://w32tex.org/ Comparing the contents of each package
    to what was shipped with Paratext for Windows showed which
    packages need to be kept.

--- update-xetex.sh
+++ update-xetex.sh
@@ -3,7 +3,7 @@
 ring1=http://ring.shibaura-it.ac.jp/archives/text/TeX/ptex-win32/current/
 server=$ring1
 
-packages="dvipdfm-w32 latex mftools pdftex-w32 platex ptex-w32 web2c-lib web2c-w32 xetex-w32"
+packages="dvipdfm-w32 web2c-lib web2c-w32 xetex-w32"
 suffix=tar.xz
 for package in $packages
 do
