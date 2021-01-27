62f57c31354f04ed8fcec557420793cce3a55bef
Bobby de Vos <bobby_devos@sil.org>
2021-01-27 14:48:59 -0700

    Automatically update TeX tree with newer TeX Live files
    In a previous commit of this file (before the renaming)
    the initial package list was stated to come from http://w32tex.org/
    in the section Minimal installation.
    
    While this is correct, there were other packages added to the list
    (dvipdfm-w32 and xetex-w32) to get PDFs from XeTeX.
    
    The current list in this file is a trimmed down version of the
    first list. The contents of each package was compared to what
    was shipped with Paratext for Windows.

--- update-xetex.bash
+++ update-xetex.bash
@@ -1,13 +1,54 @@
 #!/bin/bash
 
+# save current location
+repo=$PWD/xetex
+
+# create download location
+tmpdir=$(mktemp -d -p /tmp)
+
+# record existing files
+pushd xetex
+find -type f > $tmpdir/old.txt
+popd
+
+# possible servers
 ring1=http://ring.shibaura-it.ac.jp/archives/text/TeX/ptex-win32/current/
+
+# server to use
 server=$ring1
 
+# download and unpack TeX Live
 packages="dvipdfm-w32 web2c-lib web2c-w32 xetex-w32"
 suffix=tar.xz
+
+pushd $tmpdir
 for package in $packages
 do
-    echo Download $package.$suffix
-    echo wget $ring1/$package.$suffix
-    wget $ring1/$package.$suffix
+    tarball=$package.$suffix
+    command="wget $server/$tarball"
+    echo
+    echo Download $tarball
+    echo $command
+    $command
+    echo Unpack $tarball
+    tar -Jxf $tarball
 done
+
+# show timestamps of downloaded files
+ls -lh *.$suffix
+
+# update TeX tree
+mkdir xetex
+mv -v bin share xetex
+
+pushd xetex
+find -type f > ../new.txt
+sort ../old.txt ../new.txt | uniq -d | rsync -a -v --files-from - ./ $repo
+popd
+
+# cleanup
+popd
+rm -rf $tmpdir
+echo
+echo "Review changes and commit"
+echo
