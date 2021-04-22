902b53abb7904d9cda28b2efc35cc6202f7149b7
Mark Penny <mark_penny@sil.org>
2021-04-22 23:58:08 +1200

    Releasing 1.6.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTXprint",
-    version="1.6.1.1",
+    version="1.6.9",
     description="Typesetting using (Xe)TeX for Paratext",
     url="https://software.sil.org/ptxprint",
     author="SIL International",
