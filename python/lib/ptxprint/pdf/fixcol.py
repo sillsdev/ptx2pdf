2df63b08e4013a225c411e31600c76e26fedd2a1
Martin Hosken <martin_hosken@sil.org>
2021-06-22 10:46:41 +0700

    Add Background annotation type and Highlight annotation support

--- python/lib/ptxprint/pdf/fixcol.py
+++ python/lib/ptxprint/pdf/fixcol.py
@@ -6,7 +6,7 @@ from ptxprint.pdfrw.uncompress import uncompress
 from PIL.ImageCms import applyTransform, buildTransform
 from PIL import Image
 
-import re
+import re, os
 
 def simplefloat(s, dp=3):
     return ("{:."+str(dp)+"f}").format(s).rstrip("0").rstrip(".")
@@ -28,7 +28,8 @@ def rgb2cmyk(r, g, b):
 
 class PdfStreamParser:
 
-    def parsestream(self, rdr, strm):
+    def parsestream(self, rdr, strm, **kw):
+        self.kw = kw
         res = []
         operands = []
         toks = PdfTokens(strm)
@@ -98,6 +99,13 @@ class PageState(PdfStreamParser):
         newop = "k" if op.lower() == op else "K"
         return [simplefloat(x) for x in cmyk] + [newop]
 
+    def cm(self, op, operands):
+        ins = self.kw.get('ins', None)
+        print(ins)
+        extras = [ins] if ins is not None and len(ins) else []
+        self.kw['ins'] = None
+        return operands + [op] + extras
+
     def _addgstate(self, newgs, gs, **kw):
         res = self.gstates[gs].copy()
         self.gstates[PdfName(newgs)] = res
@@ -123,8 +131,9 @@ class PageState(PdfStreamParser):
         return newim.getpixel(0, 0)
 
 
-def fixpdfcmyk(trailer, threshold=1.):
+def fixpdfcmyk(trailer, threshold=1., inserts=[], **kw):
     for pagenum, page in enumerate(trailer.pages, 1):
+        ins = inserts[pagenum - 1] if pagenum <= len(inserts) else None
         pstate = PageState(threshold, page.Resources.ExtGState)
         instrm = page.Contents
         if not isinstance(instrm, PdfArray):
@@ -133,15 +142,23 @@ def fixpdfcmyk(trailer, threshold=1.):
             if isinstance(i, PdfIndirect):
                 i = i.real_value()
             uncompress([i])
-            strm = pstate.parsestream(trailer, i.stream)
+            strm = pstate.parsestream(trailer, i.stream, ins=ins, **kw)
             i.stream = strm
+        annots = page.Annots
+        if annots is not None:
+            for a in annots:
+                col = a.C
+                if len(col) == 3:
+                    newc = rgb2cmyk(*map(float, col))
+                    a.C = PdfArray(list(map(PdfObject, newc)))
 
 def fixhighlights(trailer, parlocs=None):
     annotlocs = {}
+    res = []
     if parlocs is not None and os.path.exists(parlocs):
         with open(parlocs) as inf:
-            for l in parlocs:
-                m = re.match("^(start|end)annot\{(.*?)\}\{(.*?)\}\{(.*?)\}")
+            for l in inf.readlines():
+                m = re.match(r"^\\(start|end)annot\{(.*?)\}\{(.*?)\}\{(.*?)\}", l)
                 if m:
                     pos = (float(m.group(3)) / 65536, float(m.group(4)) / 65536)
                     if m.group(1) == "start":
@@ -154,43 +171,70 @@ def fixhighlights(trailer, parlocs=None):
             continue
         newannots = {}
         for ann in annots:
-            if ann.SubType == "/Highlight" and ann.QuadPoints is None:
+            if ann.Subtype in ("/Highlight", "/Background") and ann.QuadPoints is None:
                 r = ann.Rect
                 if ann.Ref is not None:
                     if ann.Ref not in newannots:
                         newannots[ann.Ref] = ann
                         ann.QuadPoints = []
-                    newannots[ann.Ref].QuadPoints.append(ann.Rect)
+                    newannots[ann.Ref].QuadPoints.append(list(map(float, ann.Rect)))
             else:
                 newannots.setdefault(None, []).append(ann)
-        page.Annots = PdfArray()
-        for k, v in newannots:
+        pannots = PdfArray()
+        pres = []
+        for k, v in newannots.items():
             if k is None:
                 page.Annots.extend(v)
-            v.Subtype = v.SubType
-            v.SubType = None
             q = []
             rect = []
+            blefts = []
+            brights = []
             for i, r in enumerate(v.QuadPoints):
-                if k in annots and i == 0:
-                    xmin = annots[k][0][0]
-                else:
-                    xmin = min(r[0], r[2])
-                if k in annots and i == len(v.QuadPoints) - 1:
-                    xmax = annots[k][1][0]
-                else:
-                    xmax = max(r[0], r[2])
                 ymin = min(r[1], r[3])
                 ymax = max(r[1], r[3])
-                q += [xmin, ymax, xmax, ymax, xmax, ymin, xmin, ymin]
+                if k in annotlocs and i == 0 and ymin <= annotlocs[k][0][1] <= ymax:
+                    xmin = annotlocs[k][0][0]
+                else:
+                    xmin = min(r[0], r[2])
+                if k in annotlocs and i == len(v.QuadPoints) - 1 and ymin <= annotlocs[k][1][1] <= ymax:
+                    xmax = annotlocs[k][1][0]
+                else:
+                    xmax = max(r[0], r[2])
+                q += [xmin, ymin, xmax, ymin, xmin, ymax, xmax, ymax]
+                blefts += [(xmin, ymax), (xmin, ymin)]  # top to bottom
+                brights += [(xmax, ymax), (xmax, ymin)]
                 if i == 0:
                     rect = (xmin, ymin, xmax, ymax)
                 else:
-                    rect = (min(r[0], xmin), min(r[1], ymin), max(r[2], xmax), max(r[3], ymax))
-            v.QuadPoints = PdfArray(q)
-            v.Rect = PdfArray(rect)
-            v.Ref = None
-            page.Annots.append(v)
+                    rect = (min(rect[0], xmin), min(rect[1], ymin), max(rect[2], xmax), max(rect[3], ymax))
+            if v.Subtype == "/Highlight":
+                print(newannots.get(k, ""), annotlocs.get(k, ""), q, rect)
+                v.QuadPoints = PdfArray(q)
+                v.Rect = PdfArray(rect)
+                v.Ref = None
+                pannots.append(v)
+            elif v.Subtype == "/Background":
+                brect = []
+                for p in brights + list(reversed(blefts)):
+                    if len(brect) > 1:
+                        if brect[-2][0] == brect[-1][0] and brect[-1][0] == p[0]:
+                            brect.pop()
+                        elif p[0] != brect[-1][0]:
+                            p = (p[0], brect[-1][1])
+                    brect.append(p)
+                col = v.C
+                action = ["{} {} {} rg".format(*(simplefloat(float(c)) for c in col))]
+                action.append("{} {} m".format(simplefloat(brect[0][0]), simplefloat(brect[0][1])))
+                for p in brect[1:]:
+                    action.append("{} {} l".format(simplefloat(p[0]), simplefloat(p[1])))
+                action.append("h f")
+                print(action)
+                pres.append("\n".join(action))
+            if len(pres):
+                pres.insert(0, "q")
+                pres.append("Q")
+                page.Contents.insert(0, PdfDict(indirect=True, stream="\n".join(pres)))
+        page.Annots = pannots if len(pannots) else None
 
 def pagebbox(infile, pagenum=0):
     trailer = PdfReader(infile)
@@ -205,8 +249,8 @@ def pagebbox(infile, pagenum=0):
 def fixpdffile(infile, outfile, **kw):
     trailer = PdfReader(infile)
 
-    fixpdfcmyk(trailer, kw.get('threshold', 1.))
-    # fixhighlights(trailer)
+    fixhighlights(trailer, parlocs=kw.get('parlocs', None))
+    fixpdfcmyk(trailer, threshold=kw.get('threshold', 1.))
 
     meta = trailer.Root.Metadata
     if meta is not None:
