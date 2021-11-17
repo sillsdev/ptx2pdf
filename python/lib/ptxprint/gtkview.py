ccf55faa4d120c82e105b6d2266d329f9db6bc8b
Mark Penny <mark_penny@sil.org>
2021-11-17 22:54:59 +1300

    fix broken stylesheet after L got removed

--- python/lib/ptxprint/gtkview.py
+++ python/lib/ptxprint/gtkview.py
@@ -116,6 +116,19 @@ fr_Help l_working_dir lb_working_dir btn_about
 r_generate_selected l_generate_booklist r_generate_all c_randomPicPosn
 """.split()
 
+_ui_enable4diglot2ndary = """
+l_fontB bl_fontB l_fontI bl_fontI l_fontBI bl_fontBI 
+tb_NotesRefs lb_NotesRefs tb_general
+tb_footnotes c_includeFootnotes c_fneachnewline
+tb_xrefs     c_includeXrefs     c_xreachnewline
+c_fontFake l_fontBold s_fontBold l_fontItalic s_fontItalic
+fr_writingSystem l_textDirection fcb_textDirection fcb_script l_script
+tb_Body lb_Body
+fr_BeginEnding c_bookIntro c_introOutline c_filterGlossary c_ch1pagebreak
+fr_IncludeScripture c_mainBodyText gr_mainBodyText c_chapterNumber c_justify c_sectionHeads
+c_verseNumbers c_preventorphans c_hideEmptyVerses c_elipsizeMissingVerses
+""".split()
+
 # bx_fnOptions bx_xrOptions 
 _ui_basic = """
 r_book_module btn_chooseBibleModule lb_bibleModule
