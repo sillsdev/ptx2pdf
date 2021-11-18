eae819632a79c0b080314b140dccddd0d97852ef
Martin Hosken <martin_hosken@sil.org>
2021-11-18 12:46:18 +0700

    PDF/X-1a bans /Actions. Add \ifActions to stop them in print quality output

--- python/lib/ptxprint/snippets.py
+++ python/lib/ptxprint/snippets.py
@@ -171,6 +171,7 @@ class PDFx1aOutput(Snippet):
             extras['_gtspdfx'] = "/GTS_PDFXVersion(PDF/X-4)%\n"
         else:
             extras['_gtspdfx'] = "/GTS_PDFXVersion(PDF/X-1a:2003)%\n/GTS_PDFXConformance(PDF/X-1a:2003)%\n"
+            res += "\\Actionsfalse\n"
         if pdftype == "None":
             model.dict["/iccfpath"] = os.path.join(libpath, "sRGB.icc").replace("\\","/")
             extras['_iccnumcols'] = "3"
