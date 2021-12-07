5bfafe8a7d123c9d3bceb02dbfd69e97611c9675
Martin Hosken <martin_hosken@sil.org>
2021-12-07 15:42:04 +0700

    Change int sort to natural sort for TOCs

--- python/lib/ptxprint/toc.py
+++ python/lib/ptxprint/toc.py
@@ -66,11 +66,11 @@ class TOC:
                 res[k] = ttoc
                 if i == 2:
                     ducet = tailored("&[first primary ignorable] << 0 << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9", ducet)
-                if any(not re.match(r"^\d*$", x[i+1]) for x in tocentries):
-                    outentries = sorted(tocentries, key=lambda x:(get_sortkey(x[i+1], variable=SHIFTTRIM, ducet=ducet), refKey(x[0])))
-                else:
-                    outentries = sorted(tocentries, key=lambda x:int(x[i+1] or "0"))
-                for e in outentries:
+                def makekey(txt):
+                    return int(t) if txt.isdigit() else get_sortkey(txt, variable=SHIFTTRIM, ducet=ducet)
+                def naturalkey(txt):
+                    return [convert(c) for c in re.split('(\d+)', txt)]
+                for e in sorted(tocentries, key=naturalkey):
                     ttoc.append(e)
         return res
 
