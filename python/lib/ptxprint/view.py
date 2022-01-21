3ba36a997ec8ceb9076f4c384918c4dfd8ffdd44
Mark Penny <mark_penny@sil.org>
2022-01-21 22:09:02 +1300

    Releasing 2.0.32

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -21,7 +21,7 @@ import datetime, time
 import json
 from shutil import copyfile, copytree, move
 
-VersionStr = "2.0.31"
+VersionStr = "2.0.32"
 ConfigVersion = "2.07"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
