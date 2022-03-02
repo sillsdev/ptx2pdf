266d13e03e85ac19b42868767c79790db7669be6
Martin Hosken <martin_hosken@sil.org>
2022-03-02 13:21:24 +0700

    Override the book list for single to be the single book

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -218,7 +218,7 @@ class ViewModel:
                 return []
             res = Path(self.moduleFile).as_posix()
             return [res] if files and res else []
-        elif not local and self.bookrefs is not None:
+        elif scope != "single" and not local and self.bookrefs is not None:
             return self._bookrefsBooks(self.bookrefs, True)
         bl = RefList.fromStr(self.get("ecb_booklist", ""))
         if scope == "single" or not len(bl):
