520163bf0f9af9e99624e1bb4dcb56e0a58dab7a
Mark Penny <mark_penny@sil.org>
2022-03-03 21:51:17 +1300

    Releasing 2.1.5

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -23,7 +23,7 @@ import datetime, time
 import json
 from shutil import copyfile, copytree, move
 
-VersionStr = "2.1.4"
+VersionStr = "2.1.5"
 ConfigVersion = "2.07"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
