b134b15300d1119b7913b4576bd9569aa618e1b6
Mark Penny <mark_penny@sil.org>
2022-04-17 01:17:53 +1200

    Happy Easter everyone!

--- python/lib/ptxprint/gtkview.py
+++ python/lib/ptxprint/gtkview.py
@@ -692,6 +692,7 @@ class GtkViewModel(ViewModel):
         # self.noInternetClicked(None)
         self.onUILevelChanged(None)
         self.checkUpdates(False)
+        self.builder.get_object("gr_EasterEggs").set_visible(False)
         try:
             Gtk.main()
         except Exception as e:
@@ -3949,3 +3950,6 @@ class GtkViewModel(ViewModel):
         self.set("s_paperWeight", wght)
         self.noUpdate = False
     
+    def rgbEasterEggsClicked(self, btn):
+        self.builder.get_object("gr_EasterEggs").set_visible(self.get("c_rgbMode"))
+        
