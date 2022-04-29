1b47b343a18a38833f5991294445f0a80e48d483
Mark Penny <mark_penny@sil.org>
2022-04-30 11:04:44 +1200

    Releasing 2.1.18

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -25,8 +25,8 @@ import json
 from shutil import copyfile, copytree, move
 from difflib import Differ
 
-VersionStr = "2.1.17"
-GitVersionStr = "2.1.16-10-gda4e92b9"
+VersionStr = "2.1.18"
+GitVersionStr = "2.1.17-9-g406bdd54"
 ConfigVersion = "2.08"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
