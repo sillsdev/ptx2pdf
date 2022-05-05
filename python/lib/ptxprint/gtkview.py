a3586cdac1cb2f4041ff7b6e995731fb0fab041b
Mark Penny <mark_penny@sil.org>
2022-05-05 17:32:47 +1200

    Prepare release notes for 2.1.20

--- python/lib/ptxprint/gtkview.py
+++ python/lib/ptxprint/gtkview.py
@@ -3956,18 +3956,18 @@ class GtkViewModel(ViewModel):
         self.set("s_paperWeight", wght)
         self.noUpdate = False
     
-    def onLetterSpacingClicked(self, btn):
-        if self.loadingConfig or self.noUpdate:
-            return
-        self.noUpdate = True
-        if self.get("c_hyphenate"):
-            self.set("c_hyphenate", False)
-        self.noUpdate = False
+    # def onLetterSpacingClicked(self, btn):
+        # if self.loadingConfig or self.noUpdate:
+            # return
+        # self.noUpdate = True
+        # if self.get("c_hyphenate"):
+            # self.set("c_hyphenate", False)
+        # self.noUpdate = False
 
-    def onHyphenateClicked(self, btn):
-        if self.loadingConfig or self.noUpdate:
-            return
-        self.noUpdate = True
-        if self.get("c_letterSpacing"):
-            self.set("c_letterSpacing", False)
-        self.noUpdate = False
+    # def onHyphenateClicked(self, btn):
+        # if self.loadingConfig or self.noUpdate:
+            # return
+        # self.noUpdate = True
+        # if self.get("c_letterSpacing"):
+            # self.set("c_letterSpacing", False)
+        # self.noUpdate = False
