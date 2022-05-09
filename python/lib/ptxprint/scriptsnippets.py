6244c3332a881c442d50ac942cdd81aa565ee2c3
Martin Hosken <martin_hosken@sil.org>
2022-05-09 09:55:22 +0700

    Remove ZWSP from nonbodymarkers

--- python/lib/ptxprint/scriptsnippets.py
+++ python/lib/ptxprint/scriptsnippets.py
@@ -59,6 +59,17 @@ def onlybody(fn, bj, dat):
         res.append(fn(l))
     return "\n".join(res)
 
+def nonbody(fn, bj, dat):
+    res = []
+    for l in dat.split("\n"):
+        m = re.match(r"^\\(\S+) ", l)
+        if m:
+            if m.group(1) not in nonbodymarkers:
+                res.append(l)
+                continue
+        res.append(fn(l))
+    return "\n".join(res)
+
 class mymr(ScriptSnippet):
     dialogstruct = [
         MiniCheckButton("c_scrmymrSyllable", _("Syllable line breaking"))
@@ -70,7 +81,8 @@ class mymr(ScriptSnippet):
                (None, re.compile('([\u00AB\u2018\u201B\u201C\u201F\u2039\u2E02\u2E04\u2E09\u2E0C\u2E1C\u2E20])/'), r'\1', re.S),
                (None, re.compile('/([\u00BB\u2019\u201D\u203A\u2E03\u2E05\u2E0A\u2E0D\u2E1D\u2E21])'), r'\1', re.S),
                (None, re.compile('/([\\s\u104A\u104B])'), r'\1', re.S),
-               (None, re.compile(r'/'), "\u200B")]
+               (None, re.compile(r'/'), "\u200B"),
+               (nonbody, re.compile('\u200B'), "")]
         if model["scripts/mymr/syllables"]:
             cons = "[\u1000-\u102A\u103F\u104C-\u1055\u105A-\u105D\u1061\u1065\u1066\u106E-\u1070\u1075-\u1081" + \
                    "\u108E\u109E\u109F\uA9E0-\uA9E4\uA9E7-\uA9EF\uA9F8-\uA9FE\uAA60-\uAA6F" + \
@@ -87,7 +99,8 @@ class thai(ScriptSnippet):
     def regexes(cls, model):
         res = [(None, re.compile(r'(\s)/'), r'\1'),
                (None, re.compile('/([\\s\u0E46])'), r'\1'),
-               (None, re.compile(r'/'), "\u200B")]
+               (None, re.compile(r'/'), "\u200B"),
+               (nonbody, re.compile('\u200B'), "")]
         return res
 
 class arab(ScriptSnippet):
