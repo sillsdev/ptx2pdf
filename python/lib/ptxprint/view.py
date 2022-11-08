379fe013f5407d9340ec6c1f18c272900cebb3c0
Mark Penny <mark_penny@sil.org>
2022-11-08 23:16:56 +1300

    Releasing 2.2.38

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -28,8 +28,8 @@ from difflib import Differ
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.2.37"
-GitVersionStr = "2.2.37"
+VersionStr = "2.2.38"
+GitVersionStr = "2.2.38"
 ConfigVersion = "2.12"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTXprint",
-    version="2.2.37",
+    version="2.2.38",
     description="Typesetting using (Xe)TeX for Paratext",
     url="https://software.sil.org/ptxprint",
     author="SIL International",
