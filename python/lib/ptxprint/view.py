56fa12e01930d856b8715f569395433af3c86c84
Mark Penny <mark_penny@sil.org>
2023-01-20 23:08:11 +1300

    Releasing 2.2.48

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -29,8 +29,8 @@ from difflib import Differ
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.2.47"
-GitVersionStr = "2.2.47"
+VersionStr = "2.2.48"
+GitVersionStr = "2.2.48"
 ConfigVersion = "2.12"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
