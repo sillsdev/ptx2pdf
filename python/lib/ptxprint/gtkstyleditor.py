cd4a67d817ff72ff5de7900d0122cb6ae0544588
Martin Hosken <martin_hosken@sil.org>
2023-04-12 10:06:39 +0700

    Now unbreak non filtered

--- python/lib/ptxprint/gtkstyleditor.py
+++ python/lib/ptxprint/gtkstyleditor.py
@@ -745,8 +745,11 @@ class StyleEditorView(StyleEditor):
             del self.sheet[key]
             selection = self.treeview.get_selection()
             model, i = selection.get_selected()
-            p = model.get_path(i).convert_path_to_child_path()
-            model.get_model().row_delete(p)
+            p = model.get_path(i)
+            if isinstance(model, Gtk.TreeModelFilter):
+                p = p.convert_path_to_child_path()
+                model = model.get_model()
+            model.row_delete(p)
             self.onSelected(selection)
 
     def refreshKey(self):
