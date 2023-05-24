2c88f5f8816b055fcd40674cab822eb957916dd0
Mark Penny <mark_penny@sil.org>
2023-05-24 10:23:38 +0700

    More tweaks to the picture picker (broken at the moment)

--- python/scripts/image_selector.py
+++ python/scripts/image_selector.py
@@ -41,49 +41,45 @@ class ThumbnailWindow(Gtk.Window):
         print(folder_path)
 
         # Add thumbnails from the specified folder
-        artist_filters = self.add_thumbnails_from_folder(folder_path)
+        self.add_thumbnails_from_folder(folder_path, "GT")  # Load GT images initially
 
         # Create checkboxes for the filters
         filter_checkboxes = []
-        # filters = ["Filter 1", "Filter 2", "Filter 3"]  # Modify with your filter names
-
-        self.artist_filter_checkboxes = {}
+        artist_filters = self.get_artist_filters(folder_path)
 
         for artist in artist_filters:
             checkbox = Gtk.CheckButton(label=artist)
             checkbox.connect("toggled", self.on_filter_toggled, artist)
             filters_box.pack_start(checkbox, True, False, 0)
             filter_checkboxes.append(checkbox)
-            self.artist_filter_checkboxes[artist] = checkbox
 
-    def on_filter_toggled(self, checkbox, artist):
-        for child in self.grid.get_children():
-            thumbnail_button = child
-            thumbnail_artist = os.path.splitext(os.path.basename(thumbnail_button.get_tooltip_text()))[0][:2].upper()
+    def get_artist_filters(self, folder_path):
+        # Check if the folder exists
+        if not os.path.exists(folder_path):
+            print(f"Folder does not exist: {folder_path}")
+            return []
 
-            if thumbnail_artist == artist:
-                if checkbox.get_active():
-                    thumbnail_button.show()
-                else:
-                    thumbnail_button.hide()
+        # Read all .jpg files from the folder
+        jpg_files = [file for file in os.listdir(folder_path) if file.lower().endswith(".jpg")]
+        artists = {f[:2].upper() for f in jpg_files}
+        return sorted(artists)
 
-    def add_thumbnails_from_folder(self, folder_path):
+    def add_thumbnails_from_folder(self, folder_path, artist_filter):
         # Check if the folder exists
         if not os.path.exists(folder_path):
             print(f"Folder does not exist: {folder_path}")
             return
-        artists = {}
+
         # Read all .jpg files from the folder
         jpg_files = [file for file in os.listdir(folder_path) if file.lower().endswith(".jpg")]
-        codes = {f[:2].upper() for f in jpg_files}
-        artists = sorted(codes)
-        print(artists)
-
+        
         # Add thumbnails for each .jpg file
         for filename in jpg_files:
             file_path = os.path.join(folder_path, filename)
-            self.add_thumbnail(file_path)
-        return artists
+            thumbnail_artist = os.path.splitext(os.path.basename(filename))[0][:2].upper()
+
+            if artist_filter == thumbnail_artist:
+                self.add_thumbnail(file_path)
 
     def add_thumbnail(self, file_path):
         # Create a thumbnail button with an image
@@ -112,10 +108,7 @@ class ThumbnailWindow(Gtk.Window):
         else:
             thumbnail_height = max_size
             thumbnail_width = int(thumbnail_height * aspect_ratio)
-            
-        # Set the initial size of the thumbnail button
-        # thumbnail_button.set_size_request(150, 150)
-        
+
         # Scale the original image to the calculated thumbnail size
         thumbnail_pixbuf = original_pixbuf.scale_simple(thumbnail_width, thumbnail_height, GdkPixbuf.InterpType.BILINEAR)
         thumbnail_image.set_from_pixbuf(thumbnail_pixbuf)
@@ -135,6 +128,20 @@ class ThumbnailWindow(Gtk.Window):
             self.selected_thumbnails.remove(file_path)
 
         print("Selected thumbnails:", self.selected_thumbnails)
+
+    def on_filter_toggled(self, checkbox, artist):
+        folder_path = "D:\Reference\Bible related\BiblePictures\96dpi"
+        if checkbox.get_active():
+            self.add_thumbnails_from_folder(folder_path, artist)
+        else:
+            for child in self.grid.get_children():
+                thumbnail_button = child
+                thumbnail_artist = os.path.splitext(os.path.basename(thumbnail_button.get_tooltip_text()))[0][:2].upper()
+
+                if thumbnail_artist == artist:
+                    self.grid.remove(thumbnail_button)
+
+            self.grid.show_all()
 
 
 win = ThumbnailWindow()
