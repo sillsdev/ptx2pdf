d17b27a02b3c1842cc06fd96a397d7e342da2353
Bobby de Vos <bobby_devos@sil.org>
2023-08-21 09:32:00 -0600

    Update to TeX Live 2023 (on Windows)
    Previously, the TeX tree came from the W32TeX project https://www.w32tex.org/
    which was a repackaging of Tex Live. However this site has not been updated
    recently so the new TeX tree comes from the main TeX Live project https://tug.org/texlive/
    
    The previous commit moved files in the TeX tree to new locations as W32TeX
    and the main TeX Live project have slightly different layouts. The biggest changes
    are the location of the executables (bin/windows instead of bin) and xetex.fmt
    has moved (texmf-var/web2c/xetex instead of share/texmf-dist/web2c/xetex), thus
    creating a new directory in the xetex folder (that is, texmf-var).
    
    The file ls-R was copied to both texmf trees as it seems to be needed in texmf-var.
    The file was also added to texmf-dist for completeness.

--- bin/update-xetex.bash
+++ bin/update-xetex.bash
@@ -28,90 +28,23 @@ then
     exit 1
 fi
 
-# mapping (w32text - texlive/2023)
-pushd xetex
-
-# bin - bin/windows
-git mv -v bin windows
-mkdir bin
-git mv -v windows bin
-
-pushd share/texmf-dist
-cp -a $tmpdir/texmf-dist/ls-R .
-
-# Only in xetex/share/texmf-dist/dvipdfmx: config
-git rm -r dvipdfmx
-cp -a $tmpdir/texmf-dist/dvipdfmx .
-
-pushd fonts
-
-# Only in xetex/share/texmf-dist/fonts/tfm/public/cm: cmbcsc10.tfm
-# Only in xetex/share/texmf-dist/fonts/tfm/public/cm: cmsa10.tfm
-# Only in xetex/share/texmf-dist/fonts/tfm/public/cm: cmsytt10.tfm
-git rm tfm/public/cm/cmbcsc10.tfm
-git rm tfm/public/cm/cmsa10.tfm
-git rm tfm/public/cm/cmsytt10.tfm
-
-# Only in xetex/share/texmf-dist/fonts: conf
-git rm -r conf
-
-# Only in xetex/share/texmf-dist/fonts/map: agl
-# Only in xetex/share/texmf-dist/fonts/map/dvipdfmx: base
-# Only in xetex/share/texmf-dist/fonts/map/dvipdfmx: updmap
-git rm -r map
-mkdir map
-pushd map
-cp -a $tmpdir/texmf-dist/fonts/map/dvipdfmx .
-cp -a $tmpdir/texmf-dist/fonts/map/glyphlist .
-popd
-
-popd
-
-cp -a $tmpdir/texmf-dist/web2c/fmtutil-hdr.cnf web2c
-
-# Only in xetex/share/texmf-dist/web2c: xetex
-git rm -r web2c/xetex
-
-popd
-
-# xetex/share/texmf-dist - texmf-dist
-git mv -v share/texmf-dist texmf-dist
-
-# - texmf-var
-mkdir -p texmf-var/web2c/xetex
-pushd texmf-var
-cp -a $tmpdir/texmf-var/web2c/xetex/xetex.fmt web2c/xetex
-cp -a $tmpdir/texmf-var/ls-R .
-popd
-
-# might not be needed
-# mkdir texmf-var/fonts
-# cp -a $tmpdir/texmf-var/fonts/conf texmf-var/fonts
-
-# - texmf-config
-# cp -a $tmpdir/texmf-config .
-
-git add texmf-*
-rmdir share
-
-popd
-git commit -a -m "Move away from some W32TeX file locations"
-exit 0
-
 # save current location
 repo=$PWD/xetex
 
 # versioned files
 versioned_files="icudt*.dll msvcr*.dll"   # kpathsea*.dll
 
-# record existing files (and handle versioned files)
+# handle versioned files
 pushd xetex
 pushd bin/windows
 git rm $versioned_files
+
+# record existing files
 popd
 find -type f > $tmpdir/../old.txt
 popd
 
+# update files
 pushd $tmpdir
 find -type f > ../new.txt
 sort ../old.txt ../new.txt | uniq -d | rsync -a -v --files-from - ./ $repo
