169cfbb8d3f4ef0fcd014c69348650437de22cb3
Mark Penny <mark_penny@sil.org>
2023-08-23 23:41:58 +1200

    Releasing 2.3.37

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -31,8 +31,8 @@ from base64 import b64encode, b64decode
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.3.36"
-GitVersionStr = "2.3.36"
+VersionStr = "2.3.37"
+GitVersionStr = "2.3.37"
 ConfigVersion = "2.16"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
