4c76760f6596e3635cdb72e8956f4c7ab22887b5
Martin Hosken <martin_hosken@sil.org>
2023-08-31 10:08:03 +0100

    Refactor runsplash to find images in pyinstaller build

--- python/lib/ptxprint/runsplash.py
+++ python/lib/ptxprint/runsplash.py
@@ -5,6 +5,11 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import xml.etree.ElementTree as et
 
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    imgdir = os.path.join(sys._MEIPASS, 'ptxprint')
+else:
+    imgdir = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+
 def main():
     if len(sys.argv) > 1:
         gladefile = sys.argv[1]
@@ -14,7 +19,7 @@ def main():
     tree = et.parse(gladefile)
     for node in tree.iter():
         if node.get('name') in ('pixbuf', 'icon', 'logo'):
-            node.text = os.path.join(os.path.abspath(os.path.dirname(__file__)), node.text)
+            node.text = os.path.join(imgdir, node.text)
     xml_text = et.tostring(tree.getroot(), encoding='unicode', method='xml')
     builder = Gtk.Builder()
     builder.add_from_string(xml_text)
