2b6d7dc136d739b50a84f38be0401fb52e60b0e3
Mark Penny <mark_penny@sil.org>
2023-09-28 11:50:46 +1300

    Releasing 2.3.46

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -31,8 +31,8 @@ from base64 import b64encode, b64decode
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.3.45"
-GitVersionStr = "2.3.45"
+VersionStr = "2.3.46"
+GitVersionStr = "2.3.46"
 ConfigVersion = "2.16"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTXprint",
-    version="2.3.45",
+    version="2.3.46",
     description="Typesetting using (Xe)TeX for Paratext",
     url="https://software.sil.org/ptxprint",
     author="SIL International",
