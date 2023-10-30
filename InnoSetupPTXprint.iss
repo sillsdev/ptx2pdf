671e4cbb6c0b7a31f0faa69cdc4e7fbd6d0c1d3b
Mark Penny <mark_penny@sil.org>
2023-10-30 13:26:41 +1300

    Releasing 2.3.54

--- InnoSetupPTXprint.iss
+++ InnoSetupPTXprint.iss
@@ -1,6 +1,6 @@
 ; Inno Setup Script
 #define MyAppName "PTXprint"
-#define MyAppVersion "2.3.53"
+#define MyAppVersion "2.3.54"
 #define MyAppPublisher "SIL International"
 #define MyAppURL "http://software.sil.org/"
 #define MyAppExeName "PTXprint.exe"
--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -31,8 +31,8 @@ from base64 import b64encode, b64decode
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.3.53"
-GitVersionStr = "2.3.53"
+VersionStr = "2.3.54"
+GitVersionStr = "2.3.54"
 ConfigVersion = "2.16"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
