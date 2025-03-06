604db2be8edb07b648ecfc4fdfb592626b0dee8f
David Gardner <david_gardner@sil.org>
2025-03-06 13:53:47 +0200

    Escape any slashes in glossary footnote contents

--- python/lib/ptxprint/texmodel.py
+++ python/lib/ptxprint/texmodel.py
@@ -1527,6 +1527,7 @@ class TexModel:
                 if ge is not None:
                     for g in ge:
                         gdefn = re.sub(r"\\xt (.+?)\\xt\*", r"\1", g[2])
+                        gdefn = re.sub(r"\\", r"\\\\", gdefn)
                         gpfx = re.sub(r"~\s+"," ",g[0]) # Remove any trailing spaces from glossary item prefix
                         self.localChanges.append(makeChange(r"(\\w (.+?\|)?{} ?\\w\*)".format(g[1]), \
                                                                      r"\1\\f + {}\\fq {}: \\ft {}\\f* ".format(gpfx,g[1],gdefn), flags=regex.M))
