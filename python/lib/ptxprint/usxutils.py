17c820ba63e81ce3a94c72247f98e1af7544fb3a
Martin Hosken <martin_hosken@sil.org>
2025-03-21 15:23:46 +0700

    Fix elfactory

--- python/lib/ptxprint/usxutils.py
+++ python/lib/ptxprint/usxutils.py
@@ -280,10 +280,10 @@ class Usfm:
         self.cvaddorned = False
 
     @classmethod
-    def readfile(cls, fname, grammar=None, sheet=None):           # can also take the data straight
+    def readfile(cls, fname, grammar=None, sheet=None, elfactory=None):       # can also take the data straight
         if grammar is None:
             grammar = createGrammar(sheet if sheet is not None else [])
-        usxdoc = usfmtc.readFile(fname, informat="usfm", keepparser=True, grammar=grammar)
+        usxdoc = usfmtc.readFile(fname, informat="usfm", keepparser=True, grammar=grammar, elfactory=elfactory)
         return cls(usxdoc, usxdoc.parser, grammar=grammar)
 
     def getroot(self):
