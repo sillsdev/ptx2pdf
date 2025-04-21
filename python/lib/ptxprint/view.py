91ce4e9122f3b38075c07c690ad5407acf6a7216
Mark Penny <mark_penny@sil.org>
2025-04-21 14:33:39 +1200

    Releasing 2.8.1

--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -37,8 +37,8 @@ from typing import Dict, Optional
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.7.38"
-GitVersionStr = "2.7.38"
+VersionStr = "2.8.1"
+GitVersionStr = "2.8.1"
 ConfigVersion = "2.24"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
