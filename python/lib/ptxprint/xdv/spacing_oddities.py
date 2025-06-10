7078634edecfdffdc3d90bc04b76fec620323cab
bieropener <renske.jedidja@gmail.com>
2025-06-10 14:14:21 +0700

    Array resizing when out of bounds, itypes checked

--- python/lib/ptxprint/xdv/spacing_oddities.py
+++ python/lib/ptxprint/xdv/spacing_oddities.py
@@ -1,11 +1,12 @@
 from xdv import XDViPositionedReader 
 import numpy as np
+import math
 
 itypes = {0: 'h', 1: 'v', 2: 'width', 3: 'glyph', 4: 'space'}
 class SpacingOddities(XDViPositionedReader):
     def __init__(self, fname, diffable = False):
         super().__init__(fname, diffable)
-        self.lines = np.empty([205,30, 2]) # numpy array of lines 
+        self.lines = np.empty([200, 10, 2]) # numpy array of lines, items, width/type
         self.currline = 0 # index of current line, np array row
         self.currindex = 0 # index of current item in line, np array column
         
@@ -15,8 +16,11 @@ class SpacingOddities(XDViPositionedReader):
         return curr_pos
 
     def parmop(self, opcode, parm, data):
+        if opcode <= 142:
+            itype = 3
+        else:
+            itype = 4
         prev_pos = self.currpos()
-        itype = 5 #FIXME: find out what this is: space or glyph? and assign right itype
         super().parmop(opcode, parm, data)
 
         # value change of h indicates new glyph / space
@@ -33,7 +37,7 @@ class SpacingOddities(XDViPositionedReader):
 
     def simple(self, opcode, parm, data):
         prev_pos = self.currpos()
-        itype = 4 # FIXME: is this really always a space?s
+        itype = 4 # FIXME: is this really always a space?
         super().simple(opcode, parm, data)
         if self.h != prev_pos[0]:
             self.add_item(prev_pos, itype)
@@ -51,24 +55,24 @@ class SpacingOddities(XDViPositionedReader):
     def add_line(self, prev_pos):
         # add the width of the previous line in position 2
         self.lines[self.currline, 2, 0] = 2
-        self.lines[self.currline, 2, 1] = prev_pos[0] - self.lines[self.currline, 0, 0]
-        
+        self.lines[self.currline, 2, 1] = prev_pos[0] - self.lines[self.currline, 0, 0]    
         self.currline += 1
-        try:
-            self.lines[self.currline,0,0] = 0
-            self.lines[self.currline,0,1] = self.h
-        except IndexError:
-            #TODO: resize array here, more rows needed
-            print("more rows needed ")
+        if self.currline >= self.lines.shape[0]:
+            nlines, nitems, nwidths = self.lines.shape
+            temp_array = np.empty([math.ceil(1.5*nlines), nitems, nwidths])
+            self.lines = np.concatenate((self.lines, temp_array))
+        self.lines[self.currline,0,0] = 0
+        self.lines[self.currline,0,1] = self.h            
         # add starting coordinates of new line
         self.lines[self.currline,1,0] = 1
         self.lines[self.currline, 1, 1] = self.v
         self.currindex = 3
     
     def add_item(self, prev_pos, itype):
-        if self.currline >= self.lines.shape[0]:
-            print("array too small, more columns needed.")
-            # TODO: resize array
+        if self.currindex >= self.lines.shape[1]:
+            nlines, nitems, nwidths = self.lines.shape
+            temp_array = np.empty([nlines, math.ceil(1.5*nitems), nwidths])
+            self.lines = np.concatenate((self.lines, temp_array), axis = 1)
         # add item with its type to the line
         self.lines[self.currline, self.currindex, 0] = itype
         self.lines[self.currline, self.currindex, 1] = self.h - prev_pos[0]
@@ -77,7 +81,10 @@ class SpacingOddities(XDViPositionedReader):
     # TODO: check if xxx changes position in any way
     # TODO: check what changes when multiparm is used.
     # TODO: add set_char, since this changes h with the width of the glyph printed. (not called??)
+    # TODO: doublecheck if there's any other way in which the cursor moves
 
+    # NEXT: Perform statistics on the lines: ratio, average etc.
+    
     # FIXME: is the topt method of positioned reader actually used? Are we measuring in pt now?
 
 class Spaces:
@@ -99,13 +106,13 @@ def main():
      #   print("Don't really know what is happening but it looks cool.")
     reader = SpacingOddities("C:/Users/jedid//Documents/VSC_projects/ptx2pdf/test/projects/OGNT/local/ptxprint/Default/OGNT_Default_JHN_ptxp.xdv")
     for (opcode, data) in reader.parse():
-        if reader.pageno>2:
+        if reader.pageno>10:
             break
         pass
     # if pageno gets to x, stop. to avoid overload haha.
    # for l in reader.lines:
     #    print(l)
-    #print(reader.lines.shape)
+    print(reader.lines.shape)
     #print(reader.lines[25, 0, 1])
     #print(reader.lines[25, 1, 0])
 
