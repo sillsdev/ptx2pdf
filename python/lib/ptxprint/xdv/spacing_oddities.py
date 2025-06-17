313c6a5a8187cee385910b7e37e301733c679c17
bieropener <renske.jedidja@gmail.com>
2025-06-17 09:48:12 +0700

    Diff architecture, Lines and GlyphContent class. Bad spaces detection should work

--- python/lib/ptxprint/xdv/spacing_oddities.py
+++ python/lib/ptxprint/xdv/spacing_oddities.py
@@ -1,124 +1,149 @@
-from xdv import XDViPositionedReader 
+from ptxprint.xdv.xdv import XDViPositionedReader, Font
+from ptxprint.font import TTFont
 import math
 import re
-class SpacingOddities(XDViPositionedReader):
-    # todo: add optional parameter for max and min space.
-    def __init__(self, fname, diffable = False, slimits = (2,4)):
+
+# todo: check if we're consistent with adding gc and line objects: is currgc always -1 index?
+class SpacingOddities(XDViPositionedReader, ems = 1.2):
+    def __init__(self, fname, diffable = False):
         super().__init__(fname, diffable)
-        self.lines = [] 
-        self.currline = Line([0,0], 'first line') # fixme: add right positions for the first line.
-        # fixme: perhaps convert to pt depending on input type, will most likely be input.
-        self.smin = slimits[0]
-        self.smax = slimits[1]
-        
-    def currpos(self):
+        self.currline = None
+        self.currgc = None
+        self.ref = None
+
+    def curr_hv(self):
         curr_pos = (self.h,self.v)
         return curr_pos
 
     def parmop(self, opcode, parm, data):
-        prev_pos = self.currpos()
-        super().parmop(opcode, parm, data) 
-        self.check_changes(prev_pos, opcode)
+        prev_pos = self.curr_hv()
+        result = super().parmop(opcode, parm, data) 
+        self.handle_hv_changes(prev_pos, opcode)
+        return result
 
     def simple(self, opcode, parm, data):
-        prev_pos = self.currpos()
-        super().simple(opcode, parm, data)
-        self.check_changes(prev_pos, opcode)
+        prev_pos = self.curr_hv()
+        result = super().simple(opcode, parm, data)
+        self.handle_hv_changes(prev_pos, opcode)
+        return result
 
     def xglyphs(self, opcode, parm, data):
-        prev_pos = self.currpos()
-        super().xglyphs(opcode, parm, data)
-        self.check_changes(prev_pos, opcode)
+        prev_pos = self.curr_hv()
+        (parm, width, pos, glyphs, txt) = super().xglyphs(opcode, parm, data)
+        self.handle_hv_changes(prev_pos, opcode, gno = glyphs, gpos = pos)
+        return (parm, width, pos, glyphs, txt)
 
     def xxx(self, opcode, parm, data):
-        ref = self.readbytes(data[0])
-        ref2 = ref.decode("utf-8")
+        (txt,) = super().xxx(opcode, parm, data)
         pat = r'\((.*?)\)'
-        ref3= re.findall(pat, ref2)
-        if ref3:
-            self.currline.ref = ref3[0]
-        return (ref2,)
+        self.ref= re.findall(pat, txt)
+        #super().xxx(opcode, parm, data)
+        return (txt,)
 
-    def check_changes(self, prevpos, opcode):
+    def xfontdef(self, opcode, parm, data):
+        (k, points, flags) = data
+        # points is huge number rn.
+        # convert points to ems
+        # save ems as the space thingie for the curr glyphcontent obj
+        (k, font) = super().xfontdef(opcode, parm, data)
+        font_size = self.fonts[k].points
+        # set fontsize for current gc block
+        self.currline.content[-1].fontsize = font_size
+        print(f"size of {self.fonts[k].name} is {self.topt(font_size)}")
+        
+        return (k, font)
+        # question: should we save the h when starting new gc block, or enter first hpos when adding new glyph?
+        # so: is the hpos at start of new block same as end prev block, or is there space between blocks?
+        # and if so, under whose jurisdiction do they fall? old or new font?
+        #super().xfontdef(opcode, parm, data)
+
+    def handle_hv_changes(self, prevpos, opcode, gno = None, gpos = None):
+        # check vertical movement
         if self.v != prevpos[1]:
-            # if the current line contains min. 1 item, save endpos and add to lines list
-            if not self.currline.isEmpty():
-                self.currline.finish(self.currpos())
-                self.lines.append(self.currline)
-            self.currline = Line(self.currpos(), self.currline.ref)
-        if self.h != prevpos[0]:
-            self.currline.add_item(prevpos, self.currpos(), opcode)
-
-    def report_bad_spaces(self):
-        bad_lines = 0
-        for l in self.lines:
-            bspaces = l.find_badspaces((self.smin, self.smax))
-            if len(bspaces) >0:
-                print(f"A line in {l.ref} contains {len(bspaces)} bad spaces.")
-                bad_lines += 1
-        print(f"A total of {len(self.lines)} lines was checked. {bad_lines} lines contained bad spaces.")
-
-    def get_badspaces(self):
-        # fixme: depending on how we handle spaces, this method is unnecessary and we can just call 
-        # the badspaces function per line from another place.
-        badspace_lines = []
-        for l in self.lines:
-            bspaces = l.find_badspaces((self.smin, self.smax))
-            if len(bspaces) >0:
-                badspace_lines.append(bspaces) 
-        return badspace_lines
-
-class Line:
-    def __init__(self, startpos, ref):
-        self.starth = 0
-        self.startv = startpos[1]
-        self.width = 0
-        self.order = [] # glyph, space widths
-        self.glyphs = [] # todo: implement
-        self_gcount = 0 # todo: implement
-        self.gstart = 0 # start h-pos of glyph #todo: implement
-        self.prevline = None # keep track of previous line # todo: implement
-        self.ref = ref # todo: implement 
-
-    def isEmpty(self):
-        if len(self.order) >0:
-            return False
-        return True
-
-    def add_item(self, prevpos, newpos, opcode):
-        if opcode <143:
-            itype = 'glyph'
-        else:
-            itype = 'space'
-        if self.isEmpty():
-            self.starth = prevpos[0]
-        # fixme: there are glyphs without width, so should be added without checking width?
-        if prevpos[0] != newpos[0]:
-            iwidth = abs(newpos[0] - prevpos[0])
-            self.order.append(iwidth)
-        # todo: handle type, so save to glyphs or spaces depending on the type. See opcodes.
-
-    def finish(self, endpos):
-        self.width = abs(endpos[0] - self.starth)
-
-    def find_badspaces(self, slimits):
-        badspaces = []
-        for i in range(1, len(self.order), 2):
-            if slimits[0] <= self.order[i] <= slimits[1]:
-                pass
+            # check if line is empty (then first gc has no elements)
+            if self.currline != None:
+                if len(self.currline.content[-1].glyphs) <1:
+                    # reuse line, font stays the same, update ref
+                    self.currline.ref = self.ref
+                else:
+                    self.currline.report_badspaces(ems)
+                    self.currline.prevline = None
+                    self.currline = Line(self.ref, self.currline)
+                    self.currline.add_gc(self.font)
             else:
-                # get startpos of space by startpos line + sum of item widths up to space
-                badspaces.append((self.starth + sum(self.order[:i-1]), self.order[i]))
-        return badspaces
+                # this will be the first line
+                self.currline = Line(self.ref, Line(self.ref, None))
+                gc = GlyphContent(self.font)
+                self.currline.content.append(gc)
+        # check horizontal movement
+        if self.h != prevpos[0]:
+            # check glyph
+            if opcode < 142:
+                self.currline.content[-1].add_glyph(gno, gpos)
+
+            
+class Line: 
+    def __init__(self, ref, line):
+        self.ref = ref
+        self.content = [] # list of GlyphContent objects, index -1 contains current gc
+        self.prevline = line
+        self.vstart = 0
+        self.vrange = [0,0] # upper (min) and lower (max) v-pos of Line
+
+    def fontchange(self, font):
+        # update line vrange based on previous gc vrange
+        self.vrange[0] = min(self.vrange[0], self.content[-1].vrange[0])
+        self.vrange[1] = max(self.vrange[1], self.content[-1].vrange[1])
+        # create new gc obj with new font and add it to this line
+        gc = GlyphContent(font)
+        self.content.append(gc)
+
+    def report_badspaces(self, ems, diff = .1):
+        # needs to be per gc, since font affects max space.
+        for gc_block in self.content:
+            if gc_block.find_badspaces():
+                print(f"The line with reference {self.ref} starting at {self.get_pos()} contains badspaces")
+                break
+
+    def get_pos(self):
+        hstart = self.content[0].starts[0]
+        return (hstart, self.vstart)
+
+class GlyphContent:
+    def __init__(self, font):
+        # make ttfont with name none.
+        # todo: let fontsize be determined in one go, not save self.font
+        self.font = TTFont(None)
+        self.fontsize = 0 # in pt
+        self.glyphs = []
+        self.starts = []
+        self.vrange = (0,0) # upper and lower v of gc block 
+
+    def add_glyph(self, g_no, g_pos):
+        # fixme: method incomplete.
+        #g_no and g_pos are lists of res. numbers and 2-tuples (h,v)
+        glyph = self.font.readfont(withglyphs = True)
+        # can ask 
+        # get 
+        # todo: check max en min x en y, update gc vrange
+        self.glyphs.append(glyph)
+        #self.starts.append(pos)
+
+    def find_badspaces(self, ems, diff = .1):
+        smin, smax = self.set_slimits(ems) 
+        for i in range(len(self.starts)-2):
+            sratio = self.starts[i+1] - self.starts[i]
+            s_vs_ems = abs(sratio - ems) / ems
+            if s_vs_ems > diff:
+                return True
+        return False
 
 def main():
     reader = SpacingOddities("C:/Users/jedid//Documents/VSC_projects/ptx2pdf/test/projects/OGNT/local/ptxprint/Default/OGNT_Default_JHN_ptxp.xdv")
     for (opcode, data) in reader.parse():
-        if reader.pageno > 10:
+        if reader.pageno > 3:
             break
         pass
-    reader.report_bad_spaces()
-    
 
 if __name__ == "__main__":
     main()
