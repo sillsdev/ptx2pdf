24f70121fcec88cd03aa7ab755d3af7343b74c72
bieropener <renske.jedidja@gmail.com>
2025-06-18 10:30:47 +0700

    Runs without errors, adds gcs to Lines

--- python/lib/ptxprint/xdv/spacing_oddities.py
+++ python/lib/ptxprint/xdv/spacing_oddities.py
@@ -2,14 +2,11 @@ from ptxprint.xdv.xdv import XDViPositionedReader, Font
 from ptxprint.font import TTFont
 import math
 import re
-
-# todo: check if we're consistent with adding gc and line objects: is currgc always -1 index?
-class SpacingOddities(XDViPositionedReader, ems = 1.2):
+class SpacingOddities(XDViPositionedReader):
     def __init__(self, fname, diffable = False):
         super().__init__(fname, diffable)
-        self.currline = None
-        self.currgc = None
-        self.ref = None
+        self.curr_ref = None # reference of where we're at
+        self.lines = [] # list of Line objects 
 
     def curr_hv(self):
         curr_pos = (self.h,self.v)
@@ -17,109 +14,106 @@ class SpacingOddities(XDViPositionedReader, ems = 1.2):
 
     def parmop(self, opcode, parm, data):
         prev_pos = self.curr_hv()
-        result = super().parmop(opcode, parm, data) 
-        self.handle_hv_changes(prev_pos, opcode)
+        result = super().parmop(opcode, parm, data)
+        # question: does h change need to be handled here? It cannot be a glyph right? 
+        if self.v != prev_pos[1]:
+            self.vchange()
         return result
 
     def simple(self, opcode, parm, data):
         prev_pos = self.curr_hv()
         result = super().simple(opcode, parm, data)
-        self.handle_hv_changes(prev_pos, opcode)
+        if self.v!= prev_pos[1]:
+            self.vchange()
         return result
 
     def xglyphs(self, opcode, parm, data):
         prev_pos = self.curr_hv()
         (parm, width, pos, glyphs, txt) = super().xglyphs(opcode, parm, data)
-        self.handle_hv_changes(prev_pos, opcode, gno = glyphs, gpos = pos)
+        if self.h != prev_pos[0]: # todo: think about a minimum h difference, min width of glyphs
+            glyphs_width = self.topt(width)
+            self.lines[-1].add_glyphs(prev_pos, glyphs_width)
         return (parm, width, pos, glyphs, txt)
 
     def xxx(self, opcode, parm, data):
         (txt,) = super().xxx(opcode, parm, data)
-        pat = r'\((.*?)\)'
-        self.ref= re.findall(pat, txt)
-        #super().xxx(opcode, parm, data)
+        if re.search(r'pdf\: dest', txt):
+            self.curr_ref = re.findall(r'\((.*?)\)', txt)
+            print(f"reference is now {self.curr_ref}")
         return (txt,)
+        # todo: make into sensible string
 
     def xfontdef(self, opcode, parm, data):
         (k, points, flags) = data
-        # points is huge number rn.
-        # convert points to ems
-        # save ems as the space thingie for the curr glyphcontent obj
         (k, font) = super().xfontdef(opcode, parm, data)
-        font_size = self.fonts[k].points
-        # set fontsize for current gc block
-        self.currline.content[-1].fontsize = font_size
-        print(f"size of {self.fonts[k].name} is {self.topt(font_size)}")
-        
+        # font is an xdv Font object. Has .name (filename) and .points (size)
+        # for bad spaces, we only need the size. 
+        font_size = self.topt(font.points)
+        self.lines[-1].change_font(self.h, font_size)
+        # todo: for line collision, check if k is already in font cache
+        # todo: if not, create TTFont as attribute of the xdv-font
+        # todo: readfont() and add to font cache
+        #font.ttfont = TTFont(None, filename = font.name)
+        # question: what does the cache in TTFont class do? Since it is within the class, I thought it was an attribute of font object
+        # but it adds properties of the font to it in the __init__, so it contains multiiple fonts?
         return (k, font)
-        # question: should we save the h when starting new gc block, or enter first hpos when adding new glyph?
-        # so: is the hpos at start of new block same as end prev block, or is there space between blocks?
-        # and if so, under whose jurisdiction do they fall? old or new font?
-        #super().xfontdef(opcode, parm, data)
 
-    def handle_hv_changes(self, prevpos, opcode, gno = None, gpos = None):
-        # check vertical movement
-        if self.v != prevpos[1]:
-            # check if line is empty (then first gc has no elements)
-            if self.currline != None:
-                if len(self.currline.content[-1].glyphs) <1:
-                    # reuse line, font stays the same, update ref
-                    self.currline.ref = self.ref
-                else:
-                    self.currline.report_badspaces(ems)
-                    self.currline.prevline = None
-                    self.currline = Line(self.ref, self.currline)
-                    self.currline.add_gc(self.font)
-            else:
-                # this will be the first line
-                self.currline = Line(self.ref, Line(self.ref, None))
-                gc = GlyphContent(self.font)
-                self.currline.content.append(gc)
-        # check horizontal movement
-        if self.h != prevpos[0]:
-            # check glyph
-            if opcode < 142:
-                self.currline.content[-1].add_glyph(gno, gpos)
-
-            
+    def vchange(self):
+        # todo: handle empty lines. should we add gc now or later?
+        newline = Line(self.v, self.curr_ref)
+        self.lines.append(newline)
 class Line: 
-    def __init__(self, ref, line):
+    def __init__(self, v, ref):
         self.ref = ref
-        self.content = [] # list of GlyphContent objects, index -1 contains current gc
-        self.prevline = line
-        self.vstart = 0
-        self.vrange = [0,0] # upper (min) and lower (max) v-pos of Line
+        self.gcs = [] # list of GlyphContent objects, index -1 contains current gc
+        self.v_start = v
+        self.prev_glyph_h = 0 # to determine spaces
+        print("new line")
 
-    def fontchange(self, font):
-        # update line vrange based on previous gc vrange
-        self.vrange[0] = min(self.vrange[0], self.content[-1].vrange[0])
-        self.vrange[1] = max(self.vrange[1], self.content[-1].vrange[1])
-        # create new gc obj with new font and add it to this line
-        gc = GlyphContent(font)
-        self.content.append(gc)
+    def change_font(self, h, fontsize):
+        self.add_gc(h, fontsize)
 
-    def report_badspaces(self, ems, diff = .1):
-        # needs to be per gc, since font affects max space.
-        for gc_block in self.content:
-            if gc_block.find_badspaces():
-                print(f"The line with reference {self.ref} starting at {self.get_pos()} contains badspaces")
-                break
+    def needs_new_gc(self, prev_h, width):
+        if self.prev_glyph_h + width + 1 < prev_h + width:
+            # this is a space
+            return True
+        return False
+    
+    def add_gc(self, h, fontsize):
+        gc = GlyphContent(h, fontsize)
+        self.gcs.append(gc)
+        # todo: make sure that spaces between gc blocks are in the font of the previous block.
 
-    def get_pos(self):
-        hstart = self.content[0].starts[0]
-        return (hstart, self.vstart)
+    def add_glyphs(self, prevpos, width): # in points
+        # check if there's a space before the glyphs.
+        if abs(prevpos[0] - self.prev_glyph_h) > 1:
+            print(f"Length of gcs is now {len(self.gcs)}")
+            if len(self.gcs) !=0:
+                self.add_gc(prevpos[0], self.gcs[-1].font_size)
+            else:
+                self.add_gc(prevpos[0], None)
+        self.gcs[-1].width += width 
+        self.prev_glyph_h = prevpos[0] + width
+        # question: better way to calculate this?
+
+    def has_badspace(self):
+        for i in range (0, len(self.gcs)-1):
+            if abs(self.gcs[i+1].start_h - (self.gcs[i].start_h + self.gcs[i].width)) > 4:
+                return True
+        return False
 
 class GlyphContent:
-    def __init__(self, font):
-        # make ttfont with name none.
-        # todo: let fontsize be determined in one go, not save self.font
-        self.font = TTFont(None)
-        self.fontsize = 0 # in pt
-        self.glyphs = []
-        self.starts = []
-        self.vrange = (0,0) # upper and lower v of gc block 
+    # todo: just represents black from h to h, or width.
+    def __init__(self, h, fontsize):
+        self.start_h = h
+        self.font_size = fontsize 
+        self.width = 0
 
     def add_glyph(self, g_no, g_pos):
+        # note: this method is not in use rn
+        # todo: for every glyph, find what x&y cooridnat is relative to start of line, resolve to actual position.
+        #todo: need the y of every glyph, for calculating when things change. top of line is ymax + the offset. 
+        # size of glyph won't change, but the boundary box will.
         # fixme: method incomplete.
         #g_no and g_pos are lists of res. numbers and 2-tuples (h,v)
         glyph = self.font.readfont(withglyphs = True)
@@ -127,16 +121,7 @@ class GlyphContent:
         # get 
         # todo: check max en min x en y, update gc vrange
         self.glyphs.append(glyph)
-        #self.starts.append(pos)
-
-    def find_badspaces(self, ems, diff = .1):
-        smin, smax = self.set_slimits(ems) 
-        for i in range(len(self.starts)-2):
-            sratio = self.starts[i+1] - self.starts[i]
-            s_vs_ems = abs(sratio - ems) / ems
-            if s_vs_ems > diff:
-                return True
-        return False
+        #self.starts.append(pos)    
 
 def main():
     reader = SpacingOddities("C:/Users/jedid//Documents/VSC_projects/ptx2pdf/test/projects/OGNT/local/ptxprint/Default/OGNT_Default_JHN_ptxp.xdv")
