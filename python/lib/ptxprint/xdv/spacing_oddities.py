1c33d6974e9a7bbf60815752c92128e0fadd03b8
bieropener <renske.jedidja@gmail.com>
2025-07-04 10:56:45 +0700

    Bad space detection based on ems threshold.

--- python/lib/ptxprint/xdv/spacing_oddities.py
+++ python/lib/ptxprint/xdv/spacing_oddities.py
@@ -6,9 +6,10 @@ class SpacingOddities(XDViPositionedReader):
     def __init__(self, fname, diffable = False):
         super().__init__(fname, diffable)
         self.curr_ref = None # reference of where we're at, str
-        self.lines = [Line(self.v, self.curr_ref)] # list of Line objects
+        self.lines = [Line(self.v, self.curr_ref, None)] # list of Line objects
         self.cursor = (self.h, self.v) # location of last printed glyph
         self.ctol = 0.01 # the absolut difference tolerated to consider v change a difference
+        self.curr_fontsize = None
 
     def bop(self, opcode, parm, data):
         result = super().bop(opcode, parm, data)
@@ -21,7 +22,7 @@ class SpacingOddities(XDViPositionedReader):
             # glyphs have some width
             glyphs_width = self.topt(width)
             if self.new_line_needed(prev_pos):
-                self.lines.append(Line(prev_pos[1],self.curr_ref))
+                self.lines.append(Line(prev_pos[1],self.curr_ref, self.curr_fontsize))
             self.lines[-1].add_glyphs(prev_pos, glyphs_width)
         self.cursor = (self.h, self.v)
         return (parm, width, pos, glyphs, txt)
@@ -35,10 +36,11 @@ class SpacingOddities(XDViPositionedReader):
     def xfontdef(self, opcode, parm, data):
         # this method does not change pos
         (k, font) = super().xfontdef(opcode, parm, data)
-        font_size = self.topt(font.points)
+        self.curr_fontsize = self.topt(font.points)
         if self.new_line_needed((self.h, self.v)):
-            self.lines.append(Line(self.v, self.curr_ref))        
-        self.lines[-1].change_font((self.h, self.v), font_size)
+            self.lines.append(Line(self.v, self.curr_ref, self.curr_fontsize)) 
+        else:
+            self.lines[-1].change_font((self.h, self.v), self.curr_fontsize)
         self.cursor = (self.h,self.v)
         return (k, font)
         # todo: for line collision, check if k is already in font cache
@@ -49,7 +51,7 @@ class SpacingOddities(XDViPositionedReader):
     def new_line_needed(self, startpos):
         if len(self.lines[-1].gcs) == 0:
             # overwrite current line if it was empty.
-            self.lines[-1] = Line(startpos[1], self.curr_ref)
+            self.lines[-1] = Line(startpos[1], self.curr_ref, self.curr_fontsize)
             return False
         if math.isclose(self.cursor[1], startpos[1], abs_tol = self.ctol):
             if math.isclose(self.cursor[1], self.lines[-1].v_start, abs_tol = self.ctol):
@@ -74,31 +76,34 @@ class SpacingOddities(XDViPositionedReader):
         #         return True
         # return False                   
 class Line: 
-    def __init__(self, v, ref):
+    def __init__(self, v, ref, fontsize):
         self.ref = ref
         self.gcs = [] # list of GlyphContent objects, index -1 contains current gc
         self.v_start = v
-        self.curr_font = 0
+        self.curr_fontsize = fontsize
 
     def change_font(self, pos, fontsize):
-        self.gcs.append(GlyphContent(pos, fontsize))
-        self.curr_font = fontsize
+        self.curr_fontsize = fontsize
+        self.gcs.append(GlyphContent(pos, self.curr_fontsize))
 
     def add_glyphs(self, prevpos, w): # in points
         if len(self.gcs) == 0:
-            self.gcs.append(GlyphContent(prevpos, self.curr_font))
+            self.gcs.append(GlyphContent(prevpos, self.curr_fontsize))
         elif prevpos[0] - (self.gcs[-1].h_start + self.gcs[-1].width) >1:
             # make new gc block if there is space between glyphs this block and the previous one.
-            self.gcs.append(GlyphContent(prevpos, self.curr_font))
+            self.gcs.append(GlyphContent(prevpos, self.curr_fontsize))
         self.gcs[-1].width += w
 
     def has_badspace(self, threshold = 4):
-        # We don't count spaces at the start or end of a line
-        # todo: define space boundary based on fontsize of blocks, spaces take size of preceding block.
-        # todo: return start and width of space 
-        if len(self.gcs) >1:
-            for i in range (0, len(self.gcs)-1):
-                if self.gcs[i+1].h_start - (self.gcs[i].h_start + self.gcs[i].width) > threshold:
+        # threshold in ems
+        if len(self.gcs) > 1:
+            fontsize = self.gcs[0].font_size
+            maxspace = fontsize*threshold
+            for i in range(0, len(self.gcs)-1):
+                if self.gcs[i].font_size != fontsize:
+                    fontsize = self.gcs[0].font_size
+                    maxspace = fontsize*threshold
+                if (self.gcs[i+1].h_start - (self.gcs[i].h_start + self.gcs[i].width)) > maxspace:
                     return True
         return False
 
@@ -120,12 +125,15 @@ def main():
     #reader = SpacingOddities("C:/Users/jedid//Documents/VSC_projects/ptx2pdf/test/projects/OGNT/local/ptxprint/Default/OGNT_Default_JHN_ptxp.xdv")
     reader = SpacingOddities("C:/Users/jedid//Documents/VSC_projects/ptx2pdf/test/projects/WSGlatin/local/ptxprint/Default/WSGlatin_Default_RUT_ptxp.xdv")
     for (opcode, data) in reader.parse():
-        if reader.pageno > 1:
-            break  
-    
+        if reader.pageno > 5:
+            break 
+
+    badlines = 0
     for l in reader.lines:
-        print(f"Start line: {l.v_start}")
-    print(f"There are {len(reader.lines)} lines in total.")  
+        if l.has_badspace():
+            print(f"Ref {l.ref} at v={l.v_start} has badspace.")
+            badlines += 1
+    print(f"{badlines} out of {len(reader.lines)} lines contain a bad space.")  
 
 
 if __name__ == "__main__":
