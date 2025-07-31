61b90cc15c13be19c5da333bdf34de9b7e21f65c
bieropener <renske.jedidja@gmail.com>
2025-07-31 10:39:22 +0700

    bugfixes whitespace rivers

--- python/lib/ptxprint/xdv/spacing_oddities.py
+++ python/lib/ptxprint/xdv/spacing_oddities.py
@@ -225,34 +225,49 @@ class GlyphCluster:
         return False
     
 class Rivers:
-    def __init__(self, max_v_gap = 0.05, min_h_overlap = 0.5):
+    def __init__(self, max_v_gap = 0.05, min_h_overlap = 0.3):
         self.final_rivers = []
         self.active_rivers = []
         self.max_v_gap = max_v_gap
         self.min_h_overlap = min_h_overlap
 
     def add_line(self, line):   # check vertical gap and finish river, then add spaces
-        for river in self.active_rivers:
-            if river.vdiff(line.vmin) > self.max_v_gap*line.curr_font.points:
-                self.finish_active_river(river)
-        self.check_spaces(line.glyph_clusters)
+        spaces_info = self.get_line_spaces(line)
+        if len(self.active_rivers) == 0:
+            for space, fontsize in spaces_info:
+                self.active_rivers.append(River())   
+                self.active_rivers[-1].add(space)
+        else:
+            for river in self.active_rivers:
+                space_added = False
+                if river.vdiff(line.vmin) > self.max_v_gap*line.curr_font.points:
+                    self.finish_active_river(river)
+                else:
+                    for space, fontsize in spaces_info:
+                        if river.accepts(space, self.min_h_overlap*fontsize):
+                            river.add(space)
+                            space_added = True
+                        else:
+                            self.active_rivers.append(River())   
+                            self.active_rivers[-1].add(space)
+                    if not space_added:
+                        self.finish_active_river(river)
+        
+    def get_line_spaces(self, line):
+        spaces = []
+        for i in range(len(line.glyph_clusters)-1):
+            gc1_box = line.glyph_clusters[i].get_boundary_box()
+            gc2_box = line.glyph_clusters[i+1].get_boundary_box()
+            space = [gc1_box[2], min(gc1_box[1], gc2_box[1]), abs(gc2_box[0]-gc1_box[2]), abs(max(gc1_box[3], gc2_box[3])- min(gc1_box[1], gc2_box[1]))]                   
+            if space[2] > self.min_h_overlap*line.glyph_clusters[i].font.points:
+                spaces.append((space, line.glyph_clusters[i].font.points))
+        return spaces
         
     def finish_active_river(self, river):  # add river to final_rivers if >3 lines, create new River.
         if river.is_valid():
             self.final_rivers.append(river)
         self.active_rivers.remove(river)
-        
-    def check_spaces(self, gcs):    # iterate over space and check river acceptance, add
-        for i in range(len(gcs)-1):
-            gc1_box = gcs[i].get_boundary_box()
-            gc2_box = gcs[i+1].get_boundary_box()
-            space = [gc1_box[2], min(gc1_box[1], gc2_box[1]), abs(gc2_box[0]-gc1_box[2]), abs(max(gc1_box[3], gc2_box[3])- min(gc1_box[1], gc2_box[1]))]
-            if space[2] > self.min_h_overlap*gcs[i].font.points:  # since space between gcs takes the font of the previous gc
-                if not self.space_added(space, gcs[i].font.points):
-                    # new river.
-                    self.active_rivers.append(River())   
-                    self.active_rivers[-1].add(space)
-        
+
     def space_added(self, space, fontsize):
         added = False
         for river in self.active_rivers:
@@ -275,7 +290,7 @@ class River:
     def vdiff(self, v):
         if len(self.spaces) <1:
             return 100
-        return abs(self.spaces[-1][3] - v)
+        return abs(self.spaces[-1][1]+self.spaces[-1][3] - v)
         
     def accepts(self, space, threshold):
         if len(self.spaces) <1:
