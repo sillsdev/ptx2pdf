41e23bf0e623b02bd61c831b9f1e55b1ec5ce4fa
Mark Penny <mark_penny@sil.org>
2025-09-05 21:09:33 +1200

    Releasing 2.9.6

--- InnoSetupPTXprint.iss
+++ InnoSetupPTXprint.iss
@@ -1,6 +1,6 @@
 ; Inno Setup Script
 #define MyAppName "PTXprint"
-#define MyAppVersion "2.9.5"
+#define MyAppVersion "2.9.6"
 #define MyAppPublisher "SIL Global"
 #define MyAppURL "http://software.sil.org/"
 #define MyAppExeName "PTXprint.exe"
--- python/lib/ptxprint/view.py
+++ python/lib/ptxprint/view.py
@@ -38,8 +38,8 @@ from typing import Dict, Optional
 
 logger = logging.getLogger(__name__)
 
-VersionStr = "2.9.5"
-GitVersionStr = "2.9.5"
+VersionStr = "2.9.6"
+GitVersionStr = "2.9.6"
 ConfigVersion = "2.24"
 
 pdfre = re.compile(r".+[\\/](.+\.pdf)")
