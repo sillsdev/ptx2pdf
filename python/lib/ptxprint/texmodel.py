ad086acfa66a5ca64cff361325f849eb30c7984b
Martin Hosken <martin_hosken@sil.org>
2026-04-28 11:12:05 -0400

    Add initial markdown support requires markdown_it python module

--- python/lib/ptxprint/texmodel.py
+++ python/lib/ptxprint/texmodel.py
@@ -6,6 +6,7 @@ from ptxprint.font import TTFont
 from ptxprint.runner import checkoutput
 from ptxprint.usxutils import Usfm, Sheets
 from ptxprint.module import Module
+from ptxprint.markdown import MarkDown
 from ptxprint.utils import _, universalopen, localhdrmappings, pluralstr, multstr, \
                             chaps, books, bookcodes, allbooks, oneChbooks, f2s, cachedData, pycodedir, \
                             runChanges, booknumbers, Path, nonScriptureBooks, saferelpath, texprotect
@@ -876,16 +877,20 @@ class TexModel:
             outfpath = outfpath[:doti] + "-flat" + outfpath[doti:]
         if not noaction:
             usfms = self.printer.get_usfms()
-            mod = Module(infpath, usfms, self, text=text, changes=self.changes.get('module', []))
-            try:
-                mod.parse(self.printer.picinfos)
-            except ValueError as e:
-                self.printer.doError(f"Module error: {e}")
-                res = ""
+            if infpath.lower().endswith(".md"):
+                mod = MarkDown(infpath)
+                res = mod
             else:
-                res = mod.doc
-                if res.xml.errors:
-                    self.printer.doError("\n".join(f"{msg} in {ref} at line {pos.l} char {pos.c}" for msg, pos, ref in res.xml.errors))
+                mod = Module(infpath, usfms, self, text=text, changes=self.changes.get('module', []))
+                try:
+                    mod.parse(self.printer.picinfos)
+                except ValueError as e:
+                    self.printer.doError(f"Module error: {e}")
+                    res = ""
+                else:
+                    res = mod.doc
+                    if res.xml.errors:
+                        self.printer.doError("\n".join(f"{msg} in {ref} at line {pos.l} char {pos.c}" for msg, pos, ref in res.xml.errors))
         else:
             res = ""
         if text is not None:
