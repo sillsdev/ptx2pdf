b7107f059c51e63216934ca1768ab25832744ed3
Mark Penny <mark_penny@sil.org>
2026-09-18 18:34:19 +1200

    Fix type error when onSavePDFasClicked

--- python/lib/ptxprint/gtkview.py
+++ python/lib/ptxprint/gtkview.py
@@ -7925,6 +7925,10 @@ Thank you,
         return False
 
     def onSavePDFasClicked(self, btn): # Move me to pdf_viewer!
+        srcpath = getattr(self.pdf_viewer, "fname", None) if self.pdf_viewer is not None else None
+        if srcpath is None:
+            self.doStatus(_("No PDF is currently open to save"))
+            return
         dialog = Gtk.FileChooserDialog(
             title="Save PDF As...",
             parent = self.mainapp.win,
@@ -7932,7 +7936,7 @@ Thank you,
             buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                      Gtk.STOCK_SAVE,Gtk.ResponseType.OK))
         dialog.set_current_folder(self.userconfig.get('init', 'saveasfolder', fallback=os.path.expanduser("~")))
-        dialog.set_current_name(self.pdf_viewer.fname)
+        dialog.set_current_name(os.path.basename(srcpath))
         pdf_filter = Gtk.FileFilter()
         pdf_filter.set_name("PDF files")
         pdf_filter.add_mime_type("application/pdf")
@@ -7943,7 +7947,7 @@ Thank you,
             if not new_file_path.lower().endswith('.pdf'):
                 new_file_path += '.pdf'
             try:
-                pdffilepath = os.path.join(self.project.printPath(None), self.pdf_viewer.fname)
+                pdffilepath = os.path.join(self.project.printPath(None), srcpath)
                 copy2(pdffilepath, new_file_path)
                 self.doStatus(_("PDF saved as: ") + new_file_path)
                 self.userconfig.set('init', 'saveasfolder', os.path.dirname(new_file_path).replace("\\", "/"))
